723aeadb11f03ac57b26bf6fa7f129ad5b0baa6a
m <mohammed.mm404@gmail.com>
2026-03-06 19:25:40 +0200

    initialized flask login with placeholder login route

--- app/ext.py
+++ app/ext.py
@@ -12,4 +12,20 @@ migrate = Migrate()
 babel = Babel()
 mail = Mail()
 csrf = CSRFProtect()
-#login_manager = LoginManager()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.login_message  = 'Please login to view this page.'
+login_manager.login_message_category = 'info'
+login_manager.session_protection = 'strong'
+
+def init_flask_login(user_service):
+  from app.models import User
+  @login_manager.user_loader
+  def load_user(id):
+    return User.get(id)
+
+  @login_manager.unauthorized_handler
+  def unauthorized():
+    if request.blueprint == 'api':
+      abort(400)
+    return redirect(url_for('auth.login'))
