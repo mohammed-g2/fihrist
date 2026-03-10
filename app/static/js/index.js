def94da5bbf01193151964d9433fa78b440ad08d
m <mohammed.mm404@gmail.com>
2026-03-10 14:34:31 +0200

    static files bug fix

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,14 +1,2 @@
 
-
-function getDefaultLocale() {
-  let lang = navigator.languages;
-  if (lang !== undefined) {
-    return lang[0];
-  }
-  return lang;
-}
-
-function switchLocale(locale) {
-  return locale === 'ar' ? 'en' : 'ar';
-}
-console.log(getDefaultLocale());
+console.log('Working');
